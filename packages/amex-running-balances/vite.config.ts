5e851bf9d1f645851934acf93ed9bec60ab26149
CoolMintChocolate <70276626+CoolMintChocolate@users.noreply.github.com>
2026-03-28 15:48:10 -0700

    [vite] add more params to vite config

--- packages/amex-running-balances/vite.config.ts
+++ packages/amex-running-balances/vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 import monkey from "vite-plugin-monkey";
+import pkg from "./package.json" with { type: "json" };
+
+const PAGES_BASE =
+  "https://coolmintchocolate.github.io/userscripts/amex-running-balances";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,9 +12,11 @@ export default defineConfig({
       entry: "src/main.ts",
       userscript: {
         name: "Amex Running Balances",
+        version: pkg.version,
         icon: "https://www.americanexpress.com/content/dam/amex/us/merchant/supplies-uplift/product/images/img-WEBLOGO1-01.jpg",
         namespace: "https://github.com/CoolMintChocolate",
         match: ["https://global.americanexpress.com/activity*"],
+        downloadURL: `${PAGES_BASE}/amex-running-balances.user.js`,
       },
     }),
   ],
